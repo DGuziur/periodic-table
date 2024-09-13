5014acc47aa639567893678e7c534a938035f534
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-13 16:48:29 +0200

    create filter with debounce time

--- src/app/features/elements/pages/elements-list/elements-list.component.ts
+++ src/app/features/elements/pages/elements-list/elements-list.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { ELEMENT_DATA } from '../../config/default-elements.config';
+import { FormControl } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { debounceTime } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-elements-list',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatCardModule, MatInputModule, MatTableModule, ReactiveFormsModule],
   templateUrl: './elements-list.component.html',
   styleUrl: './elements-list.component.scss',
 })
-export class ElementsListComponent {
+export class ElementsListComponent implements OnInit {
+  protected readonly filter = new FormControl<string>('');
+
+  ngOnInit(): void {
+    this.filter.valueChanges.pipe(debounceTime(2000)).subscribe((value) => {
+      console.log(value);
+    });
+  }
   displayedColumns = ['position', 'name', 'weight', 'symbol'];
   dataSource = ELEMENT_DATA;
 }
