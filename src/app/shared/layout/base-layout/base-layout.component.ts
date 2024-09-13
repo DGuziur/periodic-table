e76a57bcaf4030890cf3331013765251f6cf31d3
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-13 21:04:15 +0200

    add dark mode toggle

--- src/app/shared/layout/base-layout/base-layout.component.ts
+++ src/app/shared/layout/base-layout/base-layout.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-base-layout',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './base-layout.component.html',
   styleUrl: './base-layout.component.scss',
 })
