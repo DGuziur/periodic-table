a47568af6cec0d7b3175ecbad196bc2874879dc7
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-13 15:55:50 +0200

    move element type to elements feature folder

--- src/app/features/elements/config/default-elements.config.ts
+++ src/app/features/elements/config/default-elements.config.ts
@@ -1,4 +1,4 @@
-import { PeriodicElement } from '../../types/periodic-element.type';
+import { PeriodicElement } from '../types/periodic-element.type';
 
 export const ELEMENT_DATA: PeriodicElement[] = [
   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
