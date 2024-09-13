cfec17f8f7002bc2f93348df3c0eec957eb3bde3
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-13 15:45:34 +0200

    create elements-list page

---
+++ src/app/features/elements/pages/elements-list/elements-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elements-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './elements-list.component.html',
+  styleUrl: './elements-list.component.scss'
+})
+export class ElementsListComponent {
+
+}
