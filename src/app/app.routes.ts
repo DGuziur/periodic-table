4a495a290eb9fec56c046de34ac69df18bc1636f
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-13 15:54:08 +0200

    create basic table view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import(
+        './features/elements/pages/elements-list/elements-list.component'
+      ).then((m) => m.ElementsListComponent),
+  },
+];
--- src/app/features/elements/pages/elements-list/elements-list.component.ts
+++ src/app/features/elements/pages/elements-list/elements-list.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { ELEMENT_DATA } from '../../config/default-elements.config';
 
 @Component({
   selector: 'app-elements-list',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './elements-list.component.html',
-  styleUrl: './elements-list.component.scss'
+  styleUrl: './elements-list.component.scss',
 })
 export class ElementsListComponent {
-
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 }
