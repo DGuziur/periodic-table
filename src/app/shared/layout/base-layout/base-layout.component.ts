d498f61be417a7c63cb3e9d9a745331d4d35f2c0
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-14 21:38:07 +0200

    finally figure out mat3 theme api

--- src/app/shared/layout/base-layout/base-layout.component.ts
+++ src/app/shared/layout/base-layout/base-layout.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-base-layout',
@@ -18,4 +19,11 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   templateUrl: './base-layout.component.html',
   styleUrl: './base-layout.component.scss',
 })
-export class BaseLayoutComponent {}
+export class BaseLayoutComponent {
+  protected readonly themeService = inject(ThemeService);
+
+  toggleTheme(event: MouseEvent): void {
+    event.stopPropagation();
+    this.themeService.toggleTheme();
+  }
+}
